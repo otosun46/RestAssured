143d70fc320f8b08f4ca6f374863dfa919f6e359
Otosun <64793578+otosun46@users.noreply.github.com>
2020-12-01 19:09:21 +0100

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ZippoTest {
+
+    @Test
+    public void test()
+    {
+        given()
+                .when()
+                .then();
+    }
+
+}
