6a91c83836c018c98a362f7045a7de94ace52d3a
Otosun <64793578+otosun46@users.noreply.github.com>
2020-12-10 20:24:10 +0100

    createCountry tamamlandi

--- src/test/java/Basqar/CountryTest.java
+++ src/test/java/Basqar/CountryTest.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 
 public class CountryTest {
@@ -31,39 +32,47 @@ public class CountryTest {
         //{"username": "daulet2030@gmail.com", "password": "TechnoStudy123@", "rememberMe": true}
 
         baseURI = "https://test.basqar.techno.study";
-        randomGenName=RandomStringUtils.randomAlphabetic(8);
-        randomGenCode=RandomStringUtils.randomAlphabetic(2);
+        randomGenName = RandomStringUtils.randomAlphabetic(8);
+        randomGenCode = RandomStringUtils.randomAlphabetic(2);
 
         Map<String, String> credentials = new HashMap<>();
         credentials.put("username", "daulet2030@gmail.com");
         credentials.put("password", "TechnoStudy123@");
         credentials.put("rememberMe", "true");
         cookies =
-        given()
-                .body(credentials)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/auth/login")
-                .then()
-                .statusCode(200)
-                .extract().response().getDetailedCookies()
+                given()
+                        .body(credentials)
+                        .contentType(ContentType.JSON)
+                        .when()
+                        .post("/auth/login")
+                        .then()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies()
         ;
         System.out.println(cookies);
     }
+
+    String id;
+
     @Test
-    public void createCountry(){
-        Country country=new Country();
+    public void createCountry() {
+        Country country = new Country();
         country.setName(randomGenName);
         country.setCode(randomGenCode);
-
-        given()
-                .body(country)
-                .contentType(ContentType.JSON)
-                .cookies(cookies)
-                .when()
-                .then()
-
-                ;
+        id =
+                given()
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .body(country)
+                        .when()
+                        .post("/school-service/api/countries")
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .body("name", equalTo(randomGenName))
+                        .extract().jsonPath().getString("id")
+        ;
+        System.out.println(id);
 
     }
 
