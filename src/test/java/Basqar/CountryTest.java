f9e2e0d92ed56f06d41451f91270aba8ee78f9ca
Otosun <64793578+otosun46@users.noreply.github.com>
2020-12-10 20:06:40 +0100

    Create CountryTest for basqar

--- src/test/java/Basqar/CountryTest.java
+++ src/test/java/Basqar/CountryTest.java
@@ -3,10 +3,12 @@
  */
 package Basqar;
 
+import Basqar.Model.Country;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookies;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -14,13 +16,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 
 public class CountryTest {
     private ResponseSpecification responseSpecification;
     private RequestSpecification requestSpecification;
-    Cookies cookes;
+    Cookies cookies;
+    private String randomGenName;
+    private String randomGenCode;
 
     @BeforeClass
     public void login() {
@@ -28,12 +31,14 @@ public class CountryTest {
         //{"username": "daulet2030@gmail.com", "password": "TechnoStudy123@", "rememberMe": true}
 
         baseURI = "https://test.basqar.techno.study";
+        randomGenName=RandomStringUtils.randomAlphabetic(8);
+        randomGenCode=RandomStringUtils.randomAlphabetic(2);
 
         Map<String, String> credentials = new HashMap<>();
         credentials.put("username", "daulet2030@gmail.com");
         credentials.put("password", "TechnoStudy123@");
         credentials.put("rememberMe", "true");
-        cookes=
+        cookies =
         given()
                 .body(credentials)
                 .contentType(ContentType.JSON)
@@ -43,10 +48,24 @@ public class CountryTest {
                 .statusCode(200)
                 .extract().response().getDetailedCookies()
         ;
-        System.out.println(cookes);
+        System.out.println(cookies);
     }
     @Test
     public void createCountry(){
+        Country country=new Country();
+        country.setName(randomGenName);
+        country.setCode(randomGenCode);
+
+        given()
+                .body(country)
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when()
+                .then()
+
+                ;
 
     }
+
+
 }
---
+++ src/test/java/Basqar/Model/Country.java
@@ -0,0 +1,34 @@
+/**
+ * @Author:Otosun Tarih :10/12/2020
+ */
+package Basqar.Model;
+
+public class Country {
+ private String id;
+ private String name;
+ private String code;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
